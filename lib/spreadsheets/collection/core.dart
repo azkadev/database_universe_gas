e314a86fc94dfd724b64696f16f8c1a434eb614b
‎ <>
2025-04-02 21:52:20 +0700

    update

--- lib/spreadsheets/collection/core.dart
+++ lib/spreadsheets/collection/core.dart
@@ -1,3 +1,37 @@
+/* <!-- START LICENSE -->
+
+
+This Software / Program / Source Code Created By Developer From Company GLOBAL CORPORATION
+Social Media:
+
+   - Youtube: https://youtube.com/@Global_Corporation 
+   - Github: https://github.com/globalcorporation
+   - TELEGRAM: https://t.me/GLOBAL_CORP_ORG_BOT
+
+All code script in here created 100% original without copy / steal from other code if we copy we add description source at from top code
+
+If you wan't edit you must add credit me (don't change)
+
+If this Software / Program / Source Code has you
+
+Jika Program ini milik anda dari hasil beli jasa developer di (Global Corporation / apapun itu dari turunan itu jika ada kesalahan / bug / ingin update segera lapor ke sub)
+
+Misal anda beli Beli source code di Slebew CORPORATION anda lapor dahulu di slebew jangan lapor di GLOBAL CORPORATION!
+
+Jika ada kendala program ini (Pastikan sebelum deal project tidak ada negosiasi harga)
+Karena jika ada negosiasi harga kemungkinan
+
+1. Software Ada yang di kurangin
+2. Informasi tidak lengkap
+3. Bantuan Tidak Bisa remote / full time (Ada jeda)
+
+Sebelum program ini sampai ke pembeli developer kami sudah melakukan testing
+
+jadi sebelum nego kami sudah melakukan berbagai konsekuensi jika nego tidak sesuai ? 
+Bukan maksud kami menipu itu karena harga yang sudah di kalkulasi + bantuan tiba tiba di potong akhirnya bantuan / software kadang tidak lengkap
+
+
+<!-- END LICENSE --> */
 import 'package:database_universe_gas/extensions/list.dart';
 import 'package:database_universe_gas/extensions/map.dart';
 import 'package:database_universe_gas/spreadsheets/scheme/scheme.dart';
@@ -45,16 +79,24 @@ class SpreadSheetsCollectionDatabaseUniverseGas {
     return;
   }
 
+/// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
   void initialized() {
     return;
   }
 
+/// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
   Map? getByIndex({
     required final int index,
   }) {
-    return spreadsheet.getSheetValues(index, 1, 1, keyLength).firstOrNull.database_universe_gas_extension_spreadsheet_toJson(keys: keys);
+    final Map? value = spreadsheet.getSheetValues(index, 1, 1, keyLength).firstOrNull.database_universe_gas_extension_spreadsheet_toJson(keys: keys);
+    if (value != null) {
+      value["@id"] = index;
+    }
+    
+    return value;
   }
 
+/// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
   bool deleteByIndex({
     required final int index,
     required final Map newValue,
@@ -74,6 +116,7 @@ class SpreadSheetsCollectionDatabaseUniverseGas {
     return true;
   }
 
+/// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
   Map setByIndex({
     required final int index,
     required final Map newValue,
@@ -95,6 +138,7 @@ class SpreadSheetsCollectionDatabaseUniverseGas {
     return newValues.database_universe_gas_extension_spreadsheet_toJson(keys: keys);
   }
 
+/// General Library Documentation Undocument By General Corporation & Global Corporation & General Developer
   Map updateByIndex({
     required final int index,
     required final Map newValue,
